0a3f2e42e6a9699822eb3f55603fc03ba26b9827
angel-vlzqz <106009767+angel-vlzqz@users.noreply.github.com>
2025-05-26 22:25:09 -0700

    finalize report for books
    still need to remove hardcoded paths, working on sys import

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import num_of_words, get_duplicates
+from stats import num_of_words, get_duplicates, print_report
 
 def get_book_text(path):
     with open(path) as f:
@@ -8,8 +8,9 @@ def main():
     franken_path = './books/frankenstein.txt'
     franken_text = get_book_text(franken_path)
     franken_duplicates = get_duplicates(franken_text)
-    print(franken_text)
-    num_of_words(franken_text)
-    print(franken_duplicates)
+    franken_num_of_words = num_of_words(franken_text)
+    print_report(franken_path, franken_num_of_words, franken_duplicates)
+
+
 
 main()
--- stats.py
+++ stats.py
@@ -6,6 +6,7 @@ def num_of_words(text):
         cnt += 1
 
     print(f'{cnt} words found in the document')
+    return cnt
 
 def get_duplicates(text):
     words = text.lower()
@@ -18,3 +19,17 @@ def get_duplicates(text):
             duplicates[character] = 1
 
     return duplicates
+
+def sort_on(dict):
+    return dict['num']
+
+def print_report(path, word_count, characters):
+    print('============ BOOKBOT ============')
+    print(f'Analyzing book found at {path}...')
+    print('----------- Word Count ----------')
+    print(f'Found {word_count} total words')
+    print('--------- Character Count -------')
+    # characters.sort(reverse=True, key=sort_on)
+    for character in characters:
+        if character.isalpha() == True:
+            print(f'{character}: {characters[character]}')
