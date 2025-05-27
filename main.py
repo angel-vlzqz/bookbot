cf2139a7c93356df451b883f339551ac65b04aa8
angel-vlzqz <106009767+angel-vlzqz@users.noreply.github.com>
2025-05-26 22:30:52 -0700

    utilize sys package to specify usage for program
    new feature removes dependency on hardcoded paths in the program

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from stats import num_of_words, get_duplicates, print_report
+import sys
+
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
 
 def main():
-    franken_path = './books/frankenstein.txt'
+    franken_path = sys.argv[1]
     franken_text = get_book_text(franken_path)
     franken_duplicates = get_duplicates(franken_text)
     franken_num_of_words = num_of_words(franken_text)
